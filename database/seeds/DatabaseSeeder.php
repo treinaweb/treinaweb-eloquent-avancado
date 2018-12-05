2f2c285ff2a22857c0be3686d38d2547492af32e
Elton Fonseca <elton869@gmail.com>
2018-12-05 15:36:06 -0200

    04 - Entendendo o uso dos seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // \DB::table('users')->insert([
+        //     'name'      => 'joao',
+        //     'email'     => 'joao@treinaweb.com.br',
+        //     'password'  => bcrypt('1234')
+        // ]);
+
+        \App\User::create([
+            'name'      => 'joao123',
+            'email'     => 'joao123@treinaweb.com.br',
+            'password'  => bcrypt('1234')
+        ]);
+
         // $this->call(UsersTableSeeder::class);
     }
 }
